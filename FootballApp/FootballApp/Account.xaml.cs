93b02c95bae93088b8655fc78ad5a8a1361775ec
AnnaGudoshnikova <goodanna_13@mail.ru>
2014-07-03 14:12:20 +0400

    were added some pages of application + added navigation between them Has fixed the bug with font

---
+++ FootballApp/FootballApp/Account.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FootballApp
+{
+    public partial class Account : PhoneApplicationPage
+    {
+        public Account()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterAccountData_Click(object sender, RoutedEventArgs e)
+        {
+            if (PhoneTextBox.Text == "" && EmailBox.Text == "")
+            {
+                MessageBox.Show("Необходимо ввести либо номер телефона либо указать почту для связи с Вами");
+            }
+
+            if (PasswordTextBox.Text != ConfirmPasswordTextBox.Text)
+            {
+                MessageBox.Show("Пароль не совпадает");
+            }
+        }
+    }
+}
---
+++ FootballApp/FootballApp/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using FootballApp.Resources;
+
+namespace FootballApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Конструктор
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Пример кода для локализации ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+   
+
+        private void СreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Account.xaml", UriKind.Relative));
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/EnterPage.xaml", UriKind.Relative));
+        }
+
+        private void EnterWithVK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EnterWithFB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Пример кода для построения локализованной панели ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Установка в качестве ApplicationBar страницы нового экземпляра ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Создание новой кнопки и установка текстового значения равным локализованной строке из AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Создание нового пункта меню с локализованной строкой из AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
