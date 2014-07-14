52142c3878a8f22ba67da51d70861598cc663881
AnnaGudoshnikova <goodanna_13@mail.ru>
2014-07-14 14:17:34 +0400

    version_14.07.2014

---
+++ FootballApp/FootballApp/CreateAccount.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+
+namespace FootballApp
+{
+    public partial class CreateAccount : PhoneApplicationPage
+    {
+        public CreateAccount()
+        {
+            InitializeComponent();
+        }
+
+        //Занести данные в Базу данных всех аккаунтов, а также сообщения об ошибках
+        private void EnterAccountData_Click(object sender, RoutedEventArgs e)
+        {
+            if (PhoneTextBox.Text == "" && EmailBox.Text == "")
+            {
+                MessageBox.Show("Необходимо ввести либо номер телефона либо указать почту для связи с Вами");
+            }
+
+            if (PasswordTextBox.Password != ConfirmPasswordTextBox.Password)
+            {
+                MessageBox.Show("Пароль не совпадает");
+            }
+        }
+        //Воврат к предыдущей странице
+        private void BackToMain_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
---
+++ FootballApp/FootballApp/EnterPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace FootballApp
+{
+    public partial class EnterPage : PhoneApplicationPage
+    {
+        public EnterPage()
+        {
+            InitializeComponent();
+        }
+
+        //Возврат к предыдущей странице 
+        private void BackToMain_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void EnterAccount_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Account.xaml", UriKind.Relative));
+        }
+    }
+}
