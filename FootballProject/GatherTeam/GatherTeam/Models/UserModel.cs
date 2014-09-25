f9c7920664764aea7b2e99aafc4e829ce3ebc34e
bashnia007 <alexanderkavokin@gmail.com>
2014-09-25 12:16:46 +0400

    add the new project

---
+++ FootballProject/GatherTeam/GatherTeam/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GatherTeam.Models
+{
+    public class UserModel
+    {
+        public string Login;
+        public string Email;
+        public string Password;
+        public string Phone;
+    }
+}
---
+++ FootballProject/GatherTeam/GatherTeam/ViewModels/UserViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GatherTeam.ViewModels
+{
+    class UserViewModel : BaseViewModel
+    {
+        private string _login;
+        private string _email;
+        private string _password;
+        private string _confirmPassword;
+        private string _phone;
+        public string Login
+        {
+            get { return _login; }
+            set
+            {
+                _login = value;
+                NotifyPropertyChanged("Login");
+            } 
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                NotifyPropertyChanged("Email");
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+                NotifyPropertyChanged("Password");
+            }
+        }
+        public string ConfirmPassword
+        {
+            get
+            {
+                return _confirmPassword;
+            }
+            set
+            {
+                _confirmPassword = value;
+                NotifyPropertyChanged("ConfirmPassword");
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return _phone;
+            }
+            set
+            {
+                _phone = value;
+                NotifyPropertyChanged("Phone");
+            }
+        }
+    }
+}
