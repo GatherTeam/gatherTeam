46e0cc39c4a9fc4f947cdd75a82bd6b31a7d4ba3
bashnia007 <alexanderkavokin@gmail.com>
2015-07-22 00:18:20 +0400

    добавлены классы для турниров

---
+++ Извра с Ажуром и Анной/gatherteamproject/gatherteamproject.WindowsPhone/DataModel/Tournaments/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gatherteamproject.DataModel.Tournaments
+{
+    class Team
+    {
+        public string Id { get; set; }
+        public List<Player> PLayers = new List<Player>();
+    }
+}
---
+++ Извра с Ажуром и Анной/gatherteamproject/gatherteamproject.WindowsPhone/DataModel/Tournaments/Tournament.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gatherteamproject.DataModel.Tournaments
+{
+    class Tournament
+    {
+        public string Id { get; set; }
+        public List<Team> Participients = new List<Team>();
+    }
+}
